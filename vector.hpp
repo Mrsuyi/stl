c4faf7ab31b55bf7f2c8b6ba3d81a0c3f470889f
Mrsuyi <mrsuyi@163.com>
2016-10-14 19:54:28 +0800

    vector basic function ok :D

--- vector.hpp
+++ vector.hpp
@@ -2,6 +2,7 @@
 
 #include <cstddef>
 #include <cstdlib>
+#include <cstring>
 #include <memory>
 
 namespace mrsuyi
@@ -27,20 +28,33 @@ public:
     using iterator = iter_impl<value_type>;
     using const_iterator = iter_impl<const value_type>;
 
-    // constructors
+    // constructor & destructor
 public:
     vector();
     vector(size_type n, const_reference val);
 
+    ~vector();
+
+    void push_back(const_reference val);
+    void pop_back();
+
     // member functions
+    void reserve(size_type size);
+    size_type capacity() const;
+    // void resize(size_type size, value_type val = value_type());
     size_type size() const;
     void clear();
 
+    reference operator[](size_type n);
+    const_reference operator[](size_type n) const;
+
+    // iterate
     iterator begin();
     iterator end();
     const_iterator end() const;
     const_iterator begin() const;
 
+    // member-variable
 private:
     pointer ptr_ = nullptr;
     size_type size_ = 0;
@@ -53,14 +67,40 @@ vector<T, Alloc>::vector()
 }
 
 template <class T, class Alloc>
-vector<T, Alloc>::vector(size_type n, const_reference val) : size_(n)
+vector<T, Alloc>::vector(size_type n, const_reference val)
 {
-    capacity_ = n;
-    ptr_ = malloc(capacity_);
+    size_t alloc = 1U;
+    for (; alloc < n; alloc *= 2);
+    reserve(alloc);
     for (size_type i = 0; i < n; ++i)
     {
         new (ptr_ + i) T(val);
     }
+    size_ = n;
+}
+
+template <class T, class Alloc>
+vector<T, Alloc>::~vector()
+{
+    for (size_type i = 0; i < size_; ++i)
+    {
+        (ptr_ + i)->~T();
+    }
+    free(ptr_);
+}
+
+template <class T, class Alloc>
+void
+vector<T, Alloc>::push_back(const_reference val)
+{
+    if (capacity_ <= size_)
+    {
+        size_t alloc = 1U;
+        for (; alloc <= size_; alloc *= 2);
+        reserve(alloc);
+    }
+    new (ptr_ + size_) T(val);
+    ++size_;
 }
 
 template <class T, class Alloc>
@@ -68,6 +108,76 @@ typename vector<T, Alloc>::size_type
 vector<T, Alloc>::size() const
 {
     return size_;
+}
+
+template <class T, class Alloc>
+void
+vector<T, Alloc>::clear()
+{
+    size_ = 0;
+}
+
+// template <class T, class Alloc>
+// void
+// vector<T, Alloc>::resize(size_type size, value_type val)
+//{
+/*}*/
+
+template <class T, class Alloc>
+void
+vector<T, Alloc>::reserve(size_type size)
+{
+    // TODO call std::realloc may cause memory problem
+    ptr_ = (pointer)std::realloc(ptr_, size * sizeof(T));
+    capacity_ = size;
+}
+
+template <class T, class Alloc>
+typename vector<T, Alloc>::size_type
+vector<T, Alloc>::capacity() const
+{
+    return capacity_;
+}
+
+template <class T, class Alloc>
+typename vector<T, Alloc>::reference vector<T, Alloc>::operator[](size_type n)
+{
+    return *(ptr_ + n);
+}
+
+template <class T, class Alloc>
+typename vector<T, Alloc>::const_reference vector<T, Alloc>::operator[](
+    size_type n) const
+{
+    return *(ptr_ + n);
+}
+
+template <class T, class Alloc>
+typename vector<T, Alloc>::iterator
+vector<T, Alloc>::begin()
+{
+    return iterator(ptr_, 0);
+}
+
+template <class T, class Alloc>
+typename vector<T, Alloc>::iterator
+vector<T, Alloc>::end()
+{
+    return iterator(ptr_, size_);
+}
+
+template <class T, class Alloc>
+typename vector<T, Alloc>::const_iterator
+vector<T, Alloc>::begin() const
+{
+    return const_iterator(ptr_, 0);
+}
+
+template <class T, class Alloc>
+typename vector<T, Alloc>::const_iterator
+vector<T, Alloc>::end() const
+{
+    return const_iterator(ptr_, size_);
 }
 
 //================================= itertor ==================================//
@@ -87,11 +197,11 @@ class vector<T, Alloc>::iter_impl
 public:
     iter_impl(pointer, difference_type = 0);
 
-    pointer operator * () const;
-    reference operator -> () const;
+    reference operator*() const;
+    pointer operator->() const;
 
-    this_t& operator ++ ();
-    bool operator != (const this_t&);
+    this_t& operator++();
+    bool operator!=(const this_t&);
 
 private:
     pointer ptr_;
@@ -100,32 +210,32 @@ private:
 
 template <class T, class Alloc>
 template <class Type>
-vector<T, Alloc>::iter_impl<Type>::iter_impl(pointer ptr, difference_type offset)
-:
-    ptr_(ptr),
-    offset_(offset)
-{}
-
-template <class T, class Alloc>
-template <class Type>
-vector<T, Alloc>::iter_impl<Type>::iter_impl::pointer
-vector<T, Alloc>::iter_impl<Type>::iter_impl::operator * () const
+vector<T, Alloc>::iter_impl<Type>::iter_impl(pointer ptr,
+                                             difference_type offset)
+    : ptr_(ptr), offset_(offset)
 {
-    return ptr_ + offset_;
 }
 
 template <class T, class Alloc>
 template <class Type>
-vector<T, Alloc>::iter_impl<Type>::iter_impl::reference
-vector<T, Alloc>::iter_impl<Type>::iter_impl::operator -> () const
+typename vector<T, Alloc>::template iter_impl<Type>::iter_impl::reference
+    vector<T, Alloc>::iter_impl<Type>::iter_impl::operator*() const
 {
     return *(ptr_ + offset_);
 }
 
 template <class T, class Alloc>
 template <class Type>
-vector<T, Alloc>::iter_impl<Type>::iter_impl::this_t&
-vector<T, Alloc>::iter_impl<Type>::iter_impl::operator ++ ()
+typename vector<T, Alloc>::template iter_impl<Type>::iter_impl::pointer
+    vector<T, Alloc>::iter_impl<Type>::iter_impl::operator->() const
+{
+    return ptr_ + offset_;
+}
+
+template <class T, class Alloc>
+template <class Type>
+typename vector<T, Alloc>::template iter_impl<Type>::iter_impl::this_t&
+    vector<T, Alloc>::iter_impl<Type>::iter_impl::operator++()
 {
     ++offset_;
 }
@@ -133,9 +243,8 @@ vector<T, Alloc>::iter_impl<Type>::iter_impl::operator ++ ()
 template <class T, class Alloc>
 template <class Type>
 bool
-vector<T, Alloc>::iter_impl<Type>::iter_impl::operator != (const this_t& other)
+vector<T, Alloc>::iter_impl<Type>::iter_impl::operator!=(const this_t& other)
 {
     return ptr_ != other.ptr_ || offset_ != other.offset_;
 }
-
 }
