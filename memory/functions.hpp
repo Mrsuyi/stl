62f81908d1eea14ed329b22f99da179dac58d04e
Mrsuyi <mrsuyi@163.com>
2016-11-07 08:22:44 +0800

    memory/functions done :D

--- memory/functions.hpp
+++ memory/functions.hpp
@@ -4,59 +4,116 @@
 
 namespace mrsuyi
 {
-template <class InputIterator, class ForwardIterator>
-ForwardIterator
-uninitialized_copy(InputIterator first, InputIterator last,
-                   ForwardIterator result)
+//==================================== copy ==================================//
+template <class InputIt, class ForwardIt>
+ForwardIt
+uninitialized_copy(InputIt first, InputIt last, ForwardIt result)
 {
     for (; first != last; ++result, ++first)
         new (static_cast<void*>(&*result))
-            typename iterator_traits<ForwardIterator>::value_type(*first);
+            typename iterator_traits<ForwardIt>::value_type(*first);
     return result;
 }
-
-template <class InputIterator, class Size, class ForwardIterator>
-ForwardIterator
-uninitialized_copy_n(InputIterator first, Size n, ForwardIterator result)
+template <class InputIt, class Size, class ForwardIt>
+ForwardIt
+uninitialized_copy_n(InputIt first, Size n, ForwardIt result)
 {
     for (; n > 0; ++result, ++first, --n)
         new (static_cast<void*>(&*result))
-            typename iterator_traits<ForwardIterator>::value_type(*first);
+            typename iterator_traits<ForwardIt>::value_type(*first);
     return result;
 }
 
-template <class InputIterator, class ForwardIterator>
-ForwardIterator
-uninitialized_move(InputIterator first, InputIterator last,
-                   ForwardIterator result)
+//==================================== move ==================================//
+template <class InputIt, class ForwardIt>
+ForwardIt
+uninitialized_move(InputIt first, InputIt last, ForwardIt result)
 {
     uninitialized_copy(make_move_iterator(first), make_move_iterator(last),
                        result);
 }
-
-template <class InputIterator, class Size, class ForwardIterator>
-ForwardIterator
-uninitialized_move_n(InputIterator first, Size n, ForwardIterator result)
+template <class InputIt, class Size, class ForwardIt>
+ForwardIt
+uninitialized_move_n(InputIt first, Size n, ForwardIt result)
 {
     uninitialized_copy_n(make_move_iterator(first), n, result);
 }
 
-template <class ForwardIterator, class T>
+//==================================== fill ==================================//
+template <class ForwardIt, class T>
 void
-uninitialized_fill(ForwardIterator first, ForwardIterator last, const T& x)
+uninitialized_fill(ForwardIt first, ForwardIt last, const T& x)
 {
     for (; first != last; ++first)
         new (static_cast<void*>(&*first))
-            typename iterator_traits<ForwardIterator>::value_type(x);
+            typename iterator_traits<ForwardIt>::value_type(x);
 }
 
-template <class ForwardIterator, class Size, class T>
-ForwardIterator
-uninitialized_fill_n(ForwardIterator first, Size n, const T& x)
+template <class ForwardIt, class Size, class T>
+ForwardIt
+uninitialized_fill_n(ForwardIt first, Size n, const T& x)
 {
     for (; n--; ++first)
         new (static_cast<void*>(&*first))
-            typename iterator_traits<ForwardIterator>::value_type(x);
+            typename iterator_traits<ForwardIt>::value_type(x);
     return first;
+}
+
+//================================ default-ctor ==============================//
+template <class ForwardIt>
+void
+uninitialized_default_construct(ForwardIt first, ForwardIt last)
+{
+    for (; first != last; ++first)
+        new (static_cast<void*>(&*first))
+            typename iterator_traits<ForwardIt>::valu_type;
+}
+
+template <class ForwardIt, class Size>
+void
+uninitialized_default_construct_n(ForwardIt first, Size n)
+{
+    for (; n > 0; ++first, --n)
+        new (static_cast<void*>(&*first))
+            typename iterator_traits<ForwardIt>::valu_type;
+}
+
+//================================ value-ctor ==============================//
+template <class ForwardIt>
+void
+uninitialized_value_construct(ForwardIt first, ForwardIt last)
+{
+    for (; first != last; ++first)
+        new (static_cast<void*>(&*first))
+            typename iterator_traits<ForwardIt>::valu_type();
+}
+
+template <class ForwardIt, class Size>
+void
+uninitialized_value_construct_n(ForwardIt first, Size n)
+{
+    for (; n > 0; ++first, --n)
+        new (static_cast<void*>(&*first))
+            typename iterator_traits<ForwardIt>::valu_type();
+}
+
+//==================================== dtor ==================================//
+template <class T>
+void
+destroy_at(T* t)
+{
+    t->~T();
+}
+template <class ForwardIt>
+void
+destory(ForwardIt first, ForwardIt last)
+{
+    for (; first != last; ++first) destroy_at(&*first);
+}
+template <class ForwardIt, class Size>
+void
+destroy_n(ForwardIt first, Size n)
+{
+    for (; n > 0; ++first, --n) destroy_at(&*first);
 }
 }
